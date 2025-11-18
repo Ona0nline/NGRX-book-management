96c9450acd389785fc55a1afa11b7d2d66791a8f
Onalerona Zwane <onzwanejhb024@student.wethinkcode.co.za>
2025-11-18 11:04:26 +0200

    Good practice of using effects

--- src/app/books/book.effects.ts
+++ src/app/books/book.effects.ts
@@ -2,19 +2,28 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import * as bookActions from './book.actions'
 import { BookService } from "./book.service";
+import { mergeMap, map, catchError, of} from "rxjs";
 
 @Injectable()
 export class BookEffects{
 
     // Actions is whatver actions are curr running in our ngrx application
     // Only run effects when a specific action appears
-    addBook$ = createEffect(() => {})
+    // will  only run when action of type AddBook appears. Define what we want to happen
+    addBook$ = createEffect(() => this.actions$.pipe(
+        ofType(bookActions.AddBook),
+        // action is this case is our Book
+        mergeMap((action => this.bookService.addBook(action)
+        .pipe(map(book => bookActions.AddBookSuccess(book)),
+        catchError((error) => of(bookActions.AddBookFailure(error)))
+    )))
+    ))
 
     constructor(
         private actions$: Actions, 
         private bookService: BookService
     ){
-
+ 
     }
 
 }
